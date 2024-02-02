61004915a8cc3d87a61bb445a1202cf79eb8846f
braydenwny <147541950+braydenwny@users.noreply.github.com>
2024-02-01 21:45:05 -0500

    Added new button features
    Added buttons that will appear after it generates a random dinner option. The first button will open a new google tab with the recipes searched for that dinner option, and the second will do the same but search for nearby restaurants that serve it

--- app.py
+++ app.py
@@ -2,5 +2,7 @@ from website import create_app
 
 app = create_app()
 
-if __name__ == '__main__':              #do not push to git for deployment
-    app.run(debug=True) 
+"""
+if __name__ == "__main__":  # do not push to git for deployment
+    app.run(debug=True)
+"""
